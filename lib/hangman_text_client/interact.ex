a97217676b44bc190ff5dc25990c801c66710639
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-21 23:01:58 +0300

    Add the general game play logic.

--- lib/hangman_text_client/interact.ex
+++ lib/hangman_text_client/interact.ex
@@ -1,11 +1,11 @@
 defmodule HangmanTextClient.Interact do
 
-  alias HangmanTextClient.State
+  alias HangmanTextClient.{State, Player}
 
   def start() do
     Hangman.new_game()
     |> setup_state()
-    |> IO.inspect
+    |> Player.play()
   end
 
   defp setup_state(game) do
