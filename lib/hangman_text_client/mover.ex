f43cad792782f1abdf17e9fad8427ba374e5c9c4
Andrei Crudu <andrei.crudu@yahoo.com>
2018-06-20 00:49:40 +0300

    Adjust hangman text client to work with the hangman game and dictionary applications.

--- lib/hangman_text_client/mover.ex
+++ lib/hangman_text_client/mover.ex
@@ -3,7 +3,7 @@ defmodule HangmanTextClient.Mover do
   alias HangmanTextClient.State
 
   def make_move(game = %State{}) do
-  { game_service, tally } = Hangman.make_move(game.game_service, game.guess)
-  %State{ game | game_service: game_service, tally: tally }
+   tally  = Hangman.make_move(game.game_service, game.guess)
+  %State{ game | tally: tally }
   end
 end
